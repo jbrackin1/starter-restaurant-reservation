2c3a0cc426c3d645f933126b5f3de3a41b3fc378
Jerod Brackin <jbrackin1@gmail.com>
2022-11-22 15:11:32 -0600

    added handleFormChange and handleSubmit to NewReservation.js

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -1,6 +1,6 @@
 import React, {useState}  from 'react'
 import {useHistory} from "react-router-dom";
-
+import ReservationForm from './ReservationForm';
 
 const NewReservation = () => {
   const defaultReservation = {
@@ -12,11 +12,33 @@ const NewReservation = () => {
     people: 0,
   };
 
-  const handleSubmit = (e) => {
-
-  }
-
   const [reservation, setReservation] = useState({...defaultReservation});
+
+  const handleFormChange = (e) => {
+    setReservation({
+      ...reservation,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+
+      reservation.people = Number(reservation.people);
+      await createReservation(reservation, controller.signal);
+      const date = reservation.reservation_date;
+      history.push(`/dashboard?date=${date}`);
+    
+  };
+
+  return (
+    <>
+      <ReservationForm
+        defaultReservation={reservation}
+        handleFormChange={handleFormChange}
+        handleSubmit={handleSubmit}
+      />
+    </>
+  );
 }
 
 export default NewReservation
